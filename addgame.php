403a795b836e263d45a76a4653371c6bfcf17c78
william <william.lonnberg@gmail.com>
2023-05-10 19:02:34 +0300

    addgame korjaus

--- addgame.php
+++ addgame.php
@@ -1,10 +1,11 @@
 
 <?php
-require "connect.php";
 session_start();
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+require "connect.php";
+if(!isset($_SESSION['adminemail'])){
+    header("Location: login.php");
+    exit;
+  }?>
 ?>
 
 <?php
