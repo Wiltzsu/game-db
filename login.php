378dc9126dd8abd629742ec2b74fce34ce806e5c
william <william.lonnberg@gmail.com>
2023-05-19 17:35:07 +0300

    matomo

--- login.php
+++ login.php
@@ -22,7 +22,22 @@ require "connect.php";
     <script src="./javascript/javascript.js"></script>
     <script src="./javascript/bootstrap.bundle.js"></script>
     <script src="./javascript/bootstrap.min.js"></script>
+
+    <script>
+      var _paq = window._paq = window._paq || [];
+      /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+      _paq.push(['trackPageView']);
+      _paq.push(['enableLinkTracking']);
+      (function() {
+        var u="//www.gamersout.com/home/wwyawxwb/public_html/";
+        _paq.push(['setTrackerUrl', u+'piwik.php']);
+        _paq.push(['setSiteId', '1']);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.async=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+      })();
+    </script>
   </head>
+
 
 <body>
 <div class="container tableborders">
