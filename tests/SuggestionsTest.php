67c3ba57bc13d82c393d705db337fbfec4cae71e
william <william.lonnberg@gmail.com>
2023-05-24 13:00:01 +0300

    tests

--- tests/SuggestionsTest.php
+++ tests/SuggestionsTest.php
@@ -5,19 +5,30 @@ class SuggestionsTest extends TestCase
 {
     public function testGetSuggestions()
     {
-        // Create a stub for the PDO class
+        // Create a mock for the PDO class
         $mockConnection = $this->getMockBuilder(PDO::class)
             ->disableOriginalConstructor()
+            ->getMock();
+
+        // Create a mock for the PDOStatement class
+        $mockStatement = $this->getMockBuilder(PDOStatement::class)
             ->getMock();
         
         // Set up the expected query and result
         $expectedTerm = 'example';
         $expectedRows = [['title' => 'Example Game 1'], ['title' => 'Example Game 2']];
-        $mockConnection->method('query')
-            ->with("SELECT title FROM games WHERE title = '$expectedTerm'")
-            ->willReturnSelf();
-        $mockConnection->method('fetchAll')
+        
+        // Set the expectations for the mock statement
+        $mockStatement->expects($this->once())
+            ->method('fetchAll')
+            ->with(PDO::FETCH_ASSOC)
             ->willReturn($expectedRows);
+
+        // Set the expectations for the mock connection
+        $mockConnection->expects($this->once())
+            ->method('query')
+            ->with("SELECT title FROM games WHERE title = '$expectedTerm'")
+            ->willReturn($mockStatement);
 
         // Replace the global $yhteys variable with the mock connection
         global $yhteys;
